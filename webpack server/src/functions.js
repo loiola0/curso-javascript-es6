1800e2da4998fa08544860511e3a76ea2b1e1cce
loiola0 <victorloiola@alu.ufc.br>
2020-04-21 12:56:36 -0300

    segundo modulo ok

---
+++ webpack server/src/functions.js
@@ -0,0 +1,9 @@
+export const idade = 23;
+
+
+export default class Usuario {
+  static info() {
+    console.log('Apenas teste, olha funcionou...');
+  }
+}
+
---
+++ webpack server/src/main.js
@@ -0,0 +1,29 @@
+/*
+//Exercicio-1
+
+//exercicio-1.1
+
+import ClasseUsuario from './functions';
+
+ClasseUsuario.info();
+
+
+
+
+//exercicio-1.2
+
+
+import {idade} from './functions';
+console.log(idade);
+
+
+//exercicio-1.3
+
+import Usuario,{idade as idadeUsuario} from './functions';
+
+console.log(Usuario.info());
+
+console.log(idadeUsuario);
+
+*/
+
